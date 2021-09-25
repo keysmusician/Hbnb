dea834afe4e19deb9077860db345b3d69875c534
Justin Masayda <justin.masayda@gmail.com>
2021-09-25 02:17:11 -0400

    Finish task 1

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,4 +1,21 @@
 $(function () {
-  $('div.amenities div.popover ul li');
-  console.log($('.amenities li'));
+  const amenityIDs = new Map();
+  // Select all amenities checkboxes; Listen for checks
+  $('div.amenities div.popover ul li input').change(function (event) {
+    if (this.checked) {
+      amenityIDs.set(this.dataset.id, this.dataset.name);
+    } else {
+      amenityIDs.delete(this.dataset.id);
+    }
+    let text = Array.from(amenityIDs.values()).join(', ');
+    // Truncate long text
+    if (text.length > 37) {
+      text = text.substring(0, 37) + '...';
+    } else if (text.length === 0) {
+      // Use a non-breaking space as a placeholder to maintain document format
+      text = '&nbsp;';
+    }
+    // Update the text of the <h4> showing selected amenities
+    $('div.amenities h4').html(text);
+  });
 });
